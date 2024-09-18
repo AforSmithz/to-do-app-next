d9e764173d34f135c3c28d739272ad8a865990d8
Abiseno <abiseno.arya@gmail.com>
2024-09-19 03:55:42 +0700

    fix: change layout and main background color

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,12 @@
 export default function Home() {
   return (
-    <main className="h-screen w-full flex items-start justify-center pt-20">
-      <section className="flex justify-between mx-80">
-        <div className=" flex justify-start items-end mr-40 font-bold text-2xl">
+    <main className="bg-[#fafaf9] h-screen w-full flex flex-col items-center justify-start gap-5 pt-20 px-80">
+      <section className="w-full flex justify-between">
+        <div className=" flex justify-start items-end font-bold text-2xl">
           Things you should be doing today...
         </div>
         <div className="flex items-center justify-center gap-8">
-          <div className="bg-to-do-blue rounded-lg px-4 py-2 flex items-center justify-center font-semibold text-to-do-white">
+          <div className="bg-to-do-blue text-to-do-white rounded-lg px-4 py-2 flex items-center justify-center font-semibold">
             Add New
           </div>
           <div className="text-to-do-red flex items-center justify-center font-semibold">
